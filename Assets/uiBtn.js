78ccbbad0ba2316d7d3671307953d267131cc6a2
Eunhee Kim <Eunhee Kim>
2015-11-24 19:11:05 +0900

    first init

---
+++ Assets/uiBtn.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+ 
+function Btnstart () {
+ 
+    Application.LoadLevel("select_ch") ;
+}
+
+function Btnhelp () {
+ 
+    Application.LoadLevel("help") ;
+}
+
+function Btnexit () {
+ 
+    Application.Quit();
+}
+
+function Btnstartgame () {
+ 
+    Application.LoadLevel("ingame") ;
+}
