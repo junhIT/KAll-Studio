6c398c6a7b8ff5de2a7ea58dc72415db36a9d30d
tjsej1324 <tjsej1324@naver.com>
2015-11-25 18:50:08 +0900

    player update

---
+++ Assets/bullet.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+private var bulletSpeed : float ;
+ 
+function Start () {
+ 
+    bulletSpeed = 50 ;
+ 
+    gameObject.GetComponent(Rigidbody).AddForce(transform.up * bulletSpeed) ;
+}
+
+
+function Update () {
+
+}
